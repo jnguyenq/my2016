026ce3449ea704abb946f83c2da54409b58e3a7d
jnguyenq <jnguyenq@gmu.edu>
2016-01-11 22:13:11 -0500

    wip

--- methods/authentication.js
+++ methods/authentication.js
@@ -10,6 +10,7 @@ exports.authenticate = function(rawCookie) {
 	knex('users').where('session_key', '96cf919f5331819c23e0235ede528818c2709ace6d96f3444a086f4d5ef50e9821f2a544b9e40f148a9178ce55d85eddb8aa04671104f2d596406dcdf07fdbb2')
 		.then(function(data) {
 			if(data.length > 0) {
+				console.log(data);
 				result = true;
 			} else {
 				result = false;
