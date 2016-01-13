5758b6392d562dd0d6bfc6410c8bea5a142e23df
jnguyenq <jnguyenq@gmu.edu>
2016-01-12 23:11:06 -0500

    wip

--- methods/authentication.js
+++ methods/authentication.js
@@ -2,15 +2,11 @@ var knex = require('../models/database').knex;
 var parseCookie = require('./cookie-parse.js');
 var Promise = require('bluebird');
 
-//Promise.promisifyAll(knex);
-
-
-exports.authenticate = Promise.method(function(rawCookie) { 
-	var cookie = parseCookie.parseCookie(rawCookie);
-	return knex('users')
-		.where('session_key', cookie)
-		.then(function(data){ 
-			return data 
-		}); 
+exports.authenticate = function(cookie) {
+	return new Promise(function(resolve, reject) {
+		    knex('users')
+			.where('session_key', cookie)
+			.then(function(data) {
+				return data;
+			});
 	});
-
